37b36b6d9805fb0c5c6834286e588796af64e738
Thong Chunlee <thong.chunlee@ababank.com>
2022-03-02 12:04:30 +0700

    អាប់ដេតកូដ

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const { StringSession } = require("telegram/sessions");
 const input = require("input");
 
 
-const authenticate = async () =>{
-  const apiId = your_api_id;
-  const apiHash = "your_api_hash";
+const authenticate = async () => {
+  const apiId = api_id;
+  const apiHash = "api_hash";
   // fill this later with the value from session.save()
   const stringSession = new StringSession("");
-  
+
   const client = new TelegramClient(stringSession, apiId, apiHash, {
     connectionRetries: 5,
   });
@@ -19,7 +19,7 @@ const authenticate = async () =>{
     // password: async () => await input.text("Please enter your password: "),
     // phoneCode: async () => await input.text("Please enter the code you received: "),
     // onError: (err) => console.log(err),
-    botAuthToken: "your_bot_token",
+    botAuthToken: "",
   });
   console.log("You should now be connected.");
   console.log("Session token: ", client.session.save());
@@ -27,30 +27,35 @@ const authenticate = async () =>{
 }
 
 const command = async () => {
-  const client = await authenticate();
+  try {
+    const client = await authenticate();
+    //Get all recent user in another group chat
+    let result = await client.invoke(
+      new Api.channels.GetParticipants({
+        channel: "channel_id",
+        filter: new Api.ChannelParticipantsRecent({}),
+        offset: 1,
+        limit: 10,
+        hash: 0,
+      })
+    );
 
-  //Get all recent user in another group chat
-  let result = await client.invoke(
-    new Api.channels.GetParticipants({
-      channel: "channel_id",
-      filter: new Api.ChannelParticipantsRecent({}),
-      offset: 1,
-      limit: 10,
-      hash: 0,
-    })
-  );
+    //Extract array of user id
+    var users = result.participants.map((e) => e.userId.value);
 
-  //Extract array of user id
-  var users = result.participants.map((e) => e.userId.value);
-
-  //Invite them to another group
-  var response = await client.invoke(
-    new Api.channels.InviteToChannel({
-      channel: "another_channel_id",
-      users: users,
-    })
-  );
-  console.log(response);
+    //Invite them to another group
+    var response = await client.invoke(
+      new Api.channels.InviteToChannel({
+        channel: "another_channel_id",
+        users: users,
+      })
+    );
+    console.log("Success: ", response.id);
+  } catch (err) {
+    console.error(err)
+  } finally {
+    process.exit();
+  }
 };
 
 command();
